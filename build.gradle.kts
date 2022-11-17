0e444b14d4a2fb42c9685a768ca391bf0cb83b96
BuildTools <unconfigured@null.spigotmc.org>
2022-11-17 18:50:28 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+
+        // As of Gradle 5.1, you can limit this to only those
+        // dependencies you expect from it
+        content {
+            includeGroup ("org.bukkit")
+            includeGroup ("org.spigotmc")
+        }
+    }
+    maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
+    maven { url = uri("https://oss.sonatype.org/content/repositories/central") }
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    compileOnly ("org.spigotmc:spigot-api:1.19-R0.1-SNAPSHOT")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ src/main/kotlin/com/maximde/randommotd/RandomMOTD.kt
@@ -0,0 +1,16 @@
+package com.maximde.randommotd
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class RandomMOTD : JavaPlugin() {
+
+    override fun onEnable() {
+        print("test")
+    }
+
+    override fun onDisable() {
+
+    }
+
+
+}
